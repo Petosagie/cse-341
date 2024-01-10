36906e0cd474454029ce7c2642df924ecc9977c5
petosagie <petosagie@gmail.com>
2024-01-10 10:44:30 +0100

    commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
  
 app.get('/', (req, res) => {
-  res.send("Hello Daddy");
+  res.send("Hello Big boy");
 });
  const port = 3000 ;
 app.listen(process.env.PORT || port, () => {
